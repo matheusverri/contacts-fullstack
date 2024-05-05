43a7b8293fe65d40786d32d70a75769298889e5e
matheusverri <matheusverri@osalim.com.br>
2024-05-05 13:23:55 -0300

    List all contacts on map

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Contact.create(name: 'Joaquim', cpf: '12345678901', phone_number: '999999999', address: 'Av. Paulista, 1000', lat: -23.564, lng: -46.654, user_id: 1)
-Contact.create(name: 'Maria', cpf: '24719073000', phone_number: '999999999', address: 'Av. Paulista, 1000', lat: -23.564, lng: -46.654, user_id: 1)
-Contact.create(name: 'Mário', cpf: '71385090065', phone_number: '999999999', address: 'Av. Paulista, 1000', lat: -23.564, lng: -46.654, user_id: 1)
+Contact.create!(name: 'Joaquim', cpf: '65304349091', phone_number: '999999999', cep: '00000000', address: 'Av. Paulista, 1000', lat: -25.42338611093241, lng: -49.27051038672994, user_id: 1)
+Contact.create!(name: 'Maria', cpf: '90490151078', phone_number: '999999999', cep: '00000000', address: 'Av. Paulista, 1000', lat: -25.36620978307509, lng: -49.18468018485261, user_id: 1)
+Contact.create!(name: 'Mário', cpf: '16560457028', phone_number: '999999999', cep: '00000000', address: 'Av. Paulista, 1000', lat: -25.53403911988444, lng: -49.20700500316308, user_id: 1)
