0dd1f4584cea2e3bc444a665ab40bbb327d69fac
matheusverri <matheusverri@osalim.com.br>
2024-05-06 10:52:22 -0300

    remove .env and applying CEP to edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Contact.create!(name: 'Joaquim', cpf: '65304349091', phone_number: '999999999', cep: '00000000', address: 'Av. Paulista, 1000', lat: -25.42338611093241, lng: -49.27051038672994, user_id: 1)
-Contact.create!(name: 'Maria', cpf: '90490151078', phone_number: '999999999', cep: '00000000', address: 'Av. Paulista, 1000', lat: -25.36620978307509, lng: -49.18468018485261, user_id: 1)
-Contact.create!(name: 'Mário', cpf: '16560457028', phone_number: '999999999', cep: '00000000', address: 'Av. Paulista, 1000', lat: -25.53403911988444, lng: -49.20700500316308, user_id: 1)
+Contact.create!(name: 'Pedro1', cpf: '406.948.466-35', phone_number: '999999999', cep: '00000000', address: 'Av. Paulista, 1000', lat: -25.42338611093241, lng: -49.27051038672994, user_id: 9)
+Contact.create!(name: 'Pedro12', cpf: '024.722.821-41', phone_number: '999999999', cep: '00000000', address: 'Av. Paulista, 1000', lat: -25.36620978307509, lng: -49.18468018485261, user_id: 9)
+Contact.create!(name: 'Pedro1234', cpf: '886.801.104-26', phone_number: '999999999', cep: '00000000', address: 'Av. Paulista, 1000', lat: -25.53403911988444, lng: -49.20700500316308, user_id: 9)
